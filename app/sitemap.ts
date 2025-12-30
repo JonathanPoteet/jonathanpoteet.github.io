d15eec2bbfc29549aeba8d8724e17097aa0764e5
Jonathan Poteet <Teetopjon@gmail.com>
2025-12-30 13:34:19 -0500

    seo fixes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://jonathanpoteet.github.io'
+  const baseUrl = 'https://jonathanpoteet.com'
 
   return [
     {
