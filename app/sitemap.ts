7c56e0dccd02118b1ab1d549eea4b0b40e0ad8d1
Jonathan Poteet <Teetopjon@gmail.com>
2025-12-30 13:25:50 -0500

    updated sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://jonathanpoteet.github.io'
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/flower`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/throwable`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ]
+}
